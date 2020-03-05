13c0009f4bf776d71e472bcb778aef0088025e86
Rostyslav Makhanko <rostyslavm@wix.com>
2020-03-06 00:33:36 +0200

    Add custom and firebaseui login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/analytics";
+import * as firebaseui from "firebaseui";
+import { createUserWithEmailAndPassword } from "./firebase";
+import "../node_modules/firebaseui/dist/firebaseui.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ubnGeEqssrFUKzdffJBTkNzxvV2p2mU",
+  authDomain: "nadezhdivka-water.firebaseapp.com",
+  databaseURL: "https://nadezhdivka-water.firebaseio.com",
+  projectId: "nadezhdivka-water",
+  storageBucket: "nadezhdivka-water.appspot.com",
+  messagingSenderId: "37254300312",
+  appId: "1:37254300312:web:a05870df81715be73f46ed",
+  measurementId: "G-2F7BJ5KDSN"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const uiConfig = {
+      signInSuccessUrl: "/success",
+      credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
+    };
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }, []);
+
+  const onRegisterButtonClick = () => {
+    console.log(email + " " + password);
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  const onEmptyButtonClick = () => {
+    console.log(111);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +60,15 @@ function App() {
         >
           Learn React
         </a>
+        <input
+          onChange={inputEmail => setEmail(inputEmail.target.value)}
+        ></input>
+        <input
+          onChange={inputPassword => setPassword(inputPassword.target.value)}
+        ></input>
+        <button onClick={onRegisterButtonClick}>Register</button>
+        <button onClick={onEmptyButtonClick}>HHH</button>
+        <div id="firebaseui-auth-container"></div>
       </header>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ubnGeEqssrFUKzdffJBTkNzxvV2p2mU",
+  authDomain: "nadezhdivka-water.firebaseapp.com",
+  databaseURL: "https://nadezhdivka-water.firebaseio.com",
+  projectId: "nadezhdivka-water",
+  storageBucket: "nadezhdivka-water.appspot.com",
+  messagingSenderId: "37254300312",
+  appId: "1:37254300312:web:a05870df81715be73f46ed",
+  measurementId: "G-2F7BJ5KDSN"
+};
+
+export function createUserWithEmailAndPassword(email, password) {
+  try {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredentials => {
+          console.log("Registered user " + userCredentials.user.email);
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode + " " + errorMessage);
+        });
+  } catch (e) {
+    console.log(e);
+    console.log('We could not register you');
+  }
+}
